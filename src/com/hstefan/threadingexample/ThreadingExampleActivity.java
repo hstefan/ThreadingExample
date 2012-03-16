ff2bd6fbb6e8b000d89454fc78f7a5b3b367f7ed
Hugo Puhlmann <hugopuhlmann@gmail.com>
2012-03-16 00:14:03 -0300

    Initial commit.

---
+++ src/com/hstefan/threadingexample/ThreadingExampleActivity.java
@@ -0,0 +1,13 @@
+package com.hstefan.threadingexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ThreadingExampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
