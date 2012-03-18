8dc1ffc8b5426a701b28115ea47df2b5a5574096
Hugo Puhlmann <hugopuhlmann@gmail.com>
2012-03-18 17:13:03 -0300

    Spawning threads for each processor, now I'll just try to find a way of joining them results and we're good to go.

--- src/com/hstefan/threadingexample/ThreadingExampleActivity.java
+++ src/com/hstefan/threadingexample/ThreadingExampleActivity.java
@@ -1,15 +1,17 @@
 package com.hstefan.threadingexample;
 
+import java.util.Arrays;
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class ThreadingExampleActivity extends Activity implements OnTaskFinishedListener<Float[]>{
 	/** Called when the activity is first created. */
 	private Float[] m_u, m_v;
+	private AsyncTask<Float[], Void, Float>[] m_Tasks;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,18 @@ public class ThreadingExampleActivity extends Activity implements OnTaskFinished
 		m_u = result[0];
 		m_v = result[1];
 		
-		Executor tExec = Executors.newFixedThreadPool(rTime.availableProcessors(), null);
+		Executor tExec = AsyncTask.THREAD_POOL_EXECUTOR;
+		int sliceSz = m_u.length/rTime.availableProcessors();
+		int numSlices = m_u.length/sliceSz;
+		
+		if(m_Tasks == null) {
+			m_Tasks = new DotProductAsyncTask[numSlices];
+		}
+		
+		for(int slices = 0; slices < numSlices; slices++) {
+			m_Tasks[slices] = new DotProductAsyncTask().executeOnExecutor(tExec, 
+					Arrays.copyOfRange(m_u, slices*sliceSz, slices*sliceSz + sliceSz),
+					Arrays.copyOfRange(m_v, slices*sliceSz, slices*sliceSz + sliceSz));
+		}
 	}
 }
