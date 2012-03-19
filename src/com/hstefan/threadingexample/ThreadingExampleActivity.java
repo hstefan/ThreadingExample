69ad25d2a1c7ca9984b3e0a694811e1110914c88
Hugo Puhlmann <hugopuhlmann@gmail.com>
2012-03-18 22:17:21 -0300

    Adding little base for future processing.

--- src/com/hstefan/threadingexample/ThreadingExampleActivity.java
+++ src/com/hstefan/threadingexample/ThreadingExampleActivity.java
@@ -6,9 +6,13 @@ import java.util.concurrent.Executor;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class ThreadingExampleActivity extends Activity implements OnTaskFinishedListener<Float[]>{
+public class ThreadingExampleActivity extends Activity implements OnTaskFinishedListener<Float[]>, OnClickListener {
+
 	/** Called when the activity is first created. */
 	private Float[] m_u, m_v;
 	private AsyncTask<Float[], Void, Float>[] m_Tasks;
@@ -18,7 +22,9 @@ public class ThreadingExampleActivity extends Activity implements OnTaskFinished
     	setContentView(R.layout.main);
         
     	Button bRun = (Button)findViewById(R.id.bRun);
-        new VectorGenerationTask().execute(new VectorGenerationData(300000, bRun, this));
+    	bRun.setOnClickListener((android.view.View.OnClickListener) this);
+        new VectorGenerationTask().setOnFinishListener(this).execute(
+        	new VectorGenerationData(300000, bRun, this));
         
         super.onCreate(savedInstanceState);
     }
@@ -29,18 +35,30 @@ public class ThreadingExampleActivity extends Activity implements OnTaskFinished
 		m_u = result[0];
 		m_v = result[1];
 		
-		Executor tExec = AsyncTask.THREAD_POOL_EXECUTOR;
 		int sliceSz = m_u.length/rTime.availableProcessors();
 		int numSlices = m_u.length/sliceSz;
 		
 		if(m_Tasks == null) {
 			m_Tasks = new DotProductAsyncTask[numSlices];
 		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		Runtime rTime  = Runtime.getRuntime();
+		calculateDotProduct(rTime.availableProcessors());
+	}
+	
+	private void calculateDotProduct(int numThreads) {
+		int sliceSz = m_u.length/numThreads;
+		int numSlices = m_u.length/sliceSz;
+		Executor tExec = AsyncTask.THREAD_POOL_EXECUTOR;
 		
-		for(int slices = 0; slices < numSlices; slices++) {
-			m_Tasks[slices] = new DotProductAsyncTask().executeOnExecutor(tExec, 
-					Arrays.copyOfRange(m_u, slices*sliceSz, slices*sliceSz + sliceSz),
-					Arrays.copyOfRange(m_v, slices*sliceSz, slices*sliceSz + sliceSz));
+		for(int slice = 0; slice < numSlices; slice++) {
+			Log.d("Thread", "Spawning thread #" + slice);
+			m_Tasks[slice] = new DotProductAsyncTask().executeOnExecutor(tExec, 
+					Arrays.copyOfRange(m_u, slice*sliceSz, slice*sliceSz + sliceSz),
+					Arrays.copyOfRange(m_v, slice*sliceSz, slice*sliceSz + sliceSz));
 		}
 	}
 }
