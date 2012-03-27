265d4563f8519e38b82b5fc694ee507c29256e00
Hugo Puhlmann <hugopuhlmann@gmail.com>
2012-03-27 13:51:25 -0300

    Givint the dot product thread maximum priority.

--- src/com/hstefan/threadingexample/DotProductAsyncTask.java
+++ src/com/hstefan/threadingexample/DotProductAsyncTask.java
@@ -3,12 +3,15 @@ package com.hstefan.threadingexample;
 import com.hstefan.threadingexample.task.Task;
 
 import android.os.AsyncTask;
+import android.os.Looper;
 import android.util.Log;
 
 public class DotProductAsyncTask extends Task<Integer, Float> {
 
 	@Override
 	public Float onRun(Integer... params) {
+		Looper.myLooper().getThread().setPriority(Thread.MAX_PRIORITY);
+		
 		Float res = 0.f;
 		DotProductThreadingSingleton l_inst = DotProductThreadingSingleton.getInstance();
 		Float u[] = l_inst.getVectorU();
